775c643229f78a3e32401f0c4732fe70e83dfe85
yotam cohen <ycohen1992@gmail.com>
2017-01-24 13:56:31 -0500

    new tutorial notebook

--- readme.txt
+++ readme.txt
@@ -1,2 +1,2 @@
-This project is still under construction, but see the included jupyter notebook on how to run it in it's current form.
+This pipeline is still under construction, but see the included jupyter notebook on how to run it in it's current form.
 
--- runpipeline.sh
+++ runpipeline.sh
@@ -1,38 +1,17 @@
-#raiddir="/Volumes/G-RAID/yotam/SAM-CANDELS_pipeline/catalogs/SC"
-raiddir="/Volumes/G-RAID/yotam/catalogs/SC/12.16"
+workingdir="/Users/yotam/CANDELizing_pipeline"
+lcfile="$workingdir/example_lightcone.dat"
+h5file="$workingdir/example_lightcone.h5"
 
-#python python_scripts/rawlightconefile_to_h5.py \
-#    lib/headerfile.txt $raiddir/goodsn.dat $raiddir/goodsn.h5
-#python python_scripts/galaxy_size_corrections.py $raiddir/goodsn.h5
-#python python_scripts/galaxy_profiles.py $raiddir/goodsn.h5
-#python python_scripts/photometric_noise_new.py $raiddir/goodsn.h5
+python python_scripts/rawlightconefile_to_h5.py \
+    lib/headerfile.txt $lcfile $h5file
 
-python python_scripts/calculate_dp.py $raiddir/cosmos.h5 \
-    ./lib/completeness_tables/cosmos_expdisk.npz \
-    ./lib/completeness_tables/cosmos_devauc.npz
-python python_scripts/detection_scheme.py $raiddir/cosmos.h5
+python python_scripts/galaxy_size_corrections.py $h5file
 
-python python_scripts/calculate_dp.py $raiddir/uds.h5 \
-    ./lib/completeness_tables/uds_expdisk.npz \
-    ./lib/completeness_tables/uds_devauc.npz
-python python_scripts/detection_scheme.py $raiddir/uds.h5
+python python_scripts/galaxy_profiles.py $h5file
 
-python python_scripts/calculate_dp.py $raiddir/egs.h5 \
-    ./lib/completeness_tables/egs_expdisk.npz \
-    ./lib/completeness_tables/egs_devauc.npz
-python python_scripts/detection_scheme.py $raiddir/egs.h5
+python python_scripts/photometric_noise_new.py $h5file
 
-python python_scripts/calculate_dp.py $raiddir/goodsn.h5 \
-    ./lib/completeness_tables/goodsn_expdisk.npz \
-    ./lib/completeness_tables/goodsn_devauc.npz
-python python_scripts/detection_scheme.py $raiddir/goodsn.h5
-
-python python_scripts/calculate_dp.py $raiddir/goodss.h5 \
+python python_scripts/calculate_dp.py $h5file \
     ./lib/completeness_tables/goodss_expdisk.npz \
     ./lib/completeness_tables/goodss_devauc.npz
-python python_scripts/detection_scheme.py $raiddir/goodss.h5
-
-
-
-
 
